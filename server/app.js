94e98a28c4b786e488057698a520fd6582dc2219
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2026-03-05 09:28:07 -0800

    Added one record to DB, random route working

--- server/app.js
+++ server/app.js
@@ -12,4 +12,20 @@ app.use(express.json());
 app.use("/api/flashcards", flashcardRoutes);
 app.use("/api/quiz", quizRoutes);
 
+// 404 HANDLER
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "Route not found",
+  });
+});
+
+// GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    error: err.message || "Internal server error",
+  });
+});
+
 module.exports = app;
--- server/routes/flashcards.js
+++ server/routes/flashcards.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { Flashcard } = require("../models");
+const Flashcard = require("../models/flashcard");
+const { asyncHandler } = require("../middleware/asyncHandler");
 
-// test route
-router.get("/random-single", (req, res) => {});
+// Get a single random flashcard
+router.get(
+  "/random",
+  asyncHandler(async (req, res) => {
+    // Get the amount of flashcards in the DB
+    const amount = await Flashcard.count();
+    console.log(`Total Count: ${amount}`);
+    // Return a random integer between 1 and amount in DB
+    const randomId = Math.floor(Math.random() * amount) + 1;
+    // Query the flashcard with the random ID
+    const randomFlashcard = await Flashcard.findByPk(randomId);
+    // Set status and parse flashcard to json
+    res.status(200).json(randomFlashcard);
+  }),
+);
 
 module.exports = router;
